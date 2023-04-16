8c249d8a9de236d79d76f51dcab1bed29f98ddee
James <bowajk@gmail.com>
2023-04-16 15:18:56 +0400

    Initial contract implementation

--- azt/lib.rs
+++ azt/lib.rs
@@ -2,141 +2,112 @@
 
 #[ink::contract]
 mod azt {
+    use ink::storage::Mapping;
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
     #[ink(storage)]
+    #[derive(Default)]
     pub struct Azt {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+        balances: Mapping<AccountId, Balance>,
+        decimals: u8,
+        total_supply: Balance,
+    }
+
+    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        InsufficientBalance,
     }
 
     impl Azt {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new(decimals: u8, total_supply: Balance) -> Self {
+            let mut balances = Mapping::default();
+            let caller = Self::env().caller();
+            // The account that calls the constructor is the contrtact creator.
+            // Deposit entire supply to the contract creator.
+            balances.insert(caller, &total_supply);
+            let total_supply = total_supply;
+
+            Self {
+                decimals,
+                balances,
+                total_supply,
+            }
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
+        pub fn decimals(&self) -> u8 {
+            self.decimals
         }
 
-        /// Simply returns the current value of our `bool`.
         #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
+        pub fn total_supply(&self) -> Balance {
+            self.total_supply
+        }
+
+        #[ink(message)]
+        pub fn balance_of(&self, account_id: AccountId) -> Balance {
+            self.balances.get(&account_id).unwrap_or_default()
+        }
+
+        #[ink(message)]
+        pub fn transfer(&mut self, to: AccountId, value: Balance) -> Result<(), Error> {
+            let from = self.env().caller();
+            let from_balance = self.balance_of(from);
+            if from_balance < value {
+                return Err(Error::InsufficientBalance);
+            }
+            self.balances.insert(from, &(from_balance - value));
+            let to_balance = self.balance_of(to);
+            self.balances.insert(to, &(to_balance + value));
+            Ok(())
         }
     }
 
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
     #[cfg(test)]
     mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
         use super::*;
 
-        /// We test if the default constructor does its job.
         #[ink::test]
-        fn default_works() {
-            let azt = Azt::default();
-            assert_eq!(azt.get(), false);
+        fn decimals_works() {
+            // valid values
+            let decimals_valid = 18;
+            let azt_token = Azt::new(decimals_valid, 100_000_000);
+            assert_eq!(azt_token.decimals(), decimals_valid);
+
+            // invalid decimals greater than 18
+            let decimals_invalid = 19;
+            let azt_token = Azt::new(decimals_invalid, 1_000_000_000);
+            assert_eq!(azt_token.decimals(), decimals_invalid);
+
+            // negative decimals
+            let decimals_negative = 1i8 as u8;
+            let azt_token = Azt::new(decimals_negative, 1_000_000_000);
+            assert_eq!(azt_token.decimals(), decimals_negative);
         }
 
-        /// We test a simple use case of our contract.
         #[ink::test]
-        fn it_works() {
-            let mut azt = Azt::new(false);
-            assert_eq!(azt.get(), false);
-            azt.flip();
-            assert_eq!(azt.get(), true);
-        }
-    }
-
-
-    /// This is how you'd write end-to-end (E2E) or integration tests for ink! contracts.
-    ///
-    /// When running these you need to make sure that you:
-    /// - Compile the tests with the `e2e-tests` feature flag enabled (`--features e2e-tests`)
-    /// - Are running a Substrate node which contains `pallet-contracts` in the background
-    #[cfg(all(test, feature = "e2e-tests"))]
-    mod e2e_tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// A helper function used for calling contract messages.
-        use ink_e2e::build_message;
-
-        /// The End-to-End test `Result` type.
-        type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
-        /// We test that we can upload and instantiate the contract using its default constructor.
-        #[ink_e2e::test]
-        async fn default_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = AztRef::default();
-
-            // When
-            let contract_account_id = client
-                .instantiate("azt", &ink_e2e::alice(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
-
-            // Then
-            let get = build_message::<AztRef>(contract_account_id.clone())
-                .call(|azt| azt.get());
-            let get_result = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
-
-            Ok(())
+        fn total_supply_works() {
+            let azt_token = Azt::new(18, 1_000_000_000);
+            assert_eq!(azt_token.total_supply(), 1_000_000_000);
         }
 
-        /// We test that we can read and write a value from the on-chain contract contract.
-        #[ink_e2e::test]
-        async fn it_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = AztRef::new(false);
-            let contract_account_id = client
-                .instantiate("azt", &ink_e2e::bob(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
+        #[ink::test]
+        fn balance_of_works() {
+            let azt_token = Azt::new(18, 1_000_000_000);
+            let accounts = ink::env::test::default_accounts::<ink::env::DefaultEnvironment>();
+            assert_eq!(azt_token.balance_of(accounts.alice), 1_000_000_000);
+            assert_eq!(azt_token.balance_of(accounts.bob), 0);
+        }
 
-            let get = build_message::<AztRef>(contract_account_id.clone())
-                .call(|azt| azt.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
+        #[ink::test]
+        fn transfer_works() {
+            let mut azt_token = Azt::new(18, 1_000_000_000);
+            let accounts = ink::env::test::default_accounts::<ink::env::DefaultEnvironment>();
 
-            // When
-            let flip = build_message::<AztRef>(contract_account_id.clone())
-                .call(|azt| azt.flip());
-            let _flip_result = client
-                .call(&ink_e2e::bob(), flip, 0, None)
-                .await
-                .expect("flip failed");
-
-            // Then
-            let get = build_message::<AztRef>(contract_account_id.clone())
-                .call(|azt| azt.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), true));
-
-            Ok(())
+            assert_eq!(azt_token.balance_of(accounts.bob), 0);
+            assert_eq!(azt_token.transfer(accounts.bob, 10), Ok(()));
+            assert_eq!(azt_token.balance_of(accounts.bob), 10);
         }
     }
 }
